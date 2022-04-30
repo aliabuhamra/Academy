4ac19b680e612d664e7e3276fe46c701223addb0
MoustafaRagab1 <88005666+MoustafaRagab1@users.noreply.github.com>
2022-04-30 13:12:28 +0400

    Add files via upload

---
+++ src/components/storeIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StoreIcon = () => {
+  return (
+    <div>
+      <div className="px-32 pb-20 pt-[270px]">
+        <img src="assets/images/storeicon.svg"  className=''/>
+      </div>
+    </div> 
+  )
+}
+
+export default StoreIcon;
