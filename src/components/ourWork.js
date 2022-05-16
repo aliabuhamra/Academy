14b38da90196169ca0191ebef67359ecf1401815
Your Name <you@example.com>
2022-05-16 10:19:21 +0300

    scroll animation

--- src/components/ourWork.js
+++ src/components/ourWork.js
@@ -2,43 +2,24 @@ import React from 'react';
 
 const OurWork = () => {
   return (
-<<<<<<< HEAD
-    <div className="py-[40px] leading-[2.6rem] font-regu font-semibold text-gray-800">
-      <div className="relative">
-        <h1 id="works" className="m-auto text-center text-5xl m-4">من أعمالنا</h1>
-=======
     <div className="py-[40px] leading-[2.6rem] font-semibold text-gray-800 ">
-      <div className="relative"> 
+      <div className="relative">
         <h1 className="m-auto text-center text-5xl sm:text-2xl m-4">من أعمالنا</h1>
->>>>>>> dc5070c9696d0b6211bf9f8ca5cfc6338a02ec46
         <div className="absolute right-[45%] left-[45%] mt-[20px] border-t-[5px] border-[#FBD675]"></div>
       </div>
 
       <div className="flex flex-wrap flex-row justify-center mt-12">
-<<<<<<< HEAD
-
-        <div className="float-left p-[25px] mt-[100px] mr-[20px] mb-[50px] ml-[20px] w-1/4 text-center	rounded-[20px] shadow-3xl ">
-          <div className="relative">
-            <h2 className="pt-4 pb-8 font-bold text-2xl text-black">محـو الأميـة</h2>
-            <p>منهاج مبسط من الصفر وشامل لمحو أميـة الكبار بالعمر وأطفال المسلميـن في المهجر، هذا المشروع استغرق ما يزيد عن العام من العمل المتواصل, ونطمح به ليكون لبنة أسـاس راسخة لأكاديميتنـا نأخذ به الأمي من الصفر باللغة العربية لإتقان مهارات مثل البرمجة والتصميم وامتهانها مستقبلا </p>
-=======
         <div className="float-left p-[25px] mt-[100px] mr-[20px] mb-[50px] ml-[20px] w-1/4 text-center	rounded-[20px] shadow-3xl ">
           <div className="relative">
             <h2 className="pt-4 pb-8 text-2xl text-black">كتاب لعبة المال أوفلاين</h2>
             <p> هذا الكتاب به حل للحظر الواقع في بعض الأقطار العربية والذي يحرم العديد من الشباب العمل على الانترنت، لهذا خرجنـا ببحث عميـق يفصل كيفية استغلال السوق المحلي بأي نوع من الأعمال سواء كان برمجة المواقـع أو ميكانيك السيارات. الكتاب مبني للفرد الأضعف بلا خلفيةويناسب ما دونه </p>
->>>>>>> dc5070c9696d0b6211bf9f8ca5cfc6338a02ec46
             <button className="font-bold text-2xl bg-[#FBD675] px-[55px] py-[4px] mt-[40px] align-middle cursor-pointer	rounded-[10px]">2021</button>
           </div>
         </div>
 
         <div className="float-left p-[25px] mt-[100px] mr-[20px] mb-[50px] ml-[20px] w-1/4 text-center	rounded-[20px] shadow-3xl ">
-<<<<<<< HEAD
           <div className="relative">
-            <h2 className="pt-4 pb-8 font-bold text-2xl text-black">TG Academy</h2>
-=======
-        <div className="relative">
-        <h2 className="pt-4 pb-8 text-2xl text-black">TG Academy</h2>
->>>>>>> dc5070c9696d0b6211bf9f8ca5cfc6338a02ec46
+            <h2 className="pt-4 pb-8 text-2xl text-black">TG Academy</h2>
             <p>في 2021 خّرجنـا حتى الآن ما يزيـد عن    150 مبرمج ومصمم، هذه الأرقـام المتواضعة ساعدتنا على بناء نظام بطاقة استيعابية يصل لـ 10 آلاف طالب في نفس الوقـت و بأكثر من 14 تخصـص نادر، سيتم تشغيل المشـروع منتصـف عام 2022 تدريجيا على أن ينطـلق بكامل طاقتـه مع مطلـع 2023 </p>
             <button className="font-bold text-2xl bg-[#FBD675] px-[55px] py-[4px] mt-[80px] align-middle cursor-pointer	rounded-[10px]">2021</button>
           </div>
@@ -46,7 +27,7 @@ const OurWork = () => {
 
         <div className="float-left p-[25px] mt-[100px] mr-[20px] mb-[50px] ml-[20px] w-1/4 text-center	rounded-[20px] shadow-3xl ">
           <div className="relative">
-          <h2 className="pt-4 pb-8 text-2xl text-black">محـو الأميـة</h2>
+            <h2 className="pt-4 pb-8 text-2xl text-black">محـو الأميـة</h2>
             <p>منهاج مبسط من الصفر وشامل لمحو أميـة الكبار بالعمر وأطفال المسلميـن في المهجر، هذا المشروع استغرق ما يزيد عن العام من العمل المتواصل, ونطمح به ليكون لبنة أسـاس راسخة لأكاديميتنـا نأخذ به الأمي من الصفر باللغة العربية لإتقان مهارات مثل البرمجة والتصميم وامتهانها مستقبلا </p>
             <button className="font-bold text-2xl bg-[#FBD675] px-[55px] py-[4px] mt-[40px] align-middle cursor-pointer	rounded-[10px]">2022</button>
           </div>
@@ -54,49 +35,29 @@ const OurWork = () => {
 
         <div className="float-left p-[25px] mt-[100px] mr-[20px] mb-[50px] ml-[20px] w-1/4 text-center	rounded-[20px] shadow-3xl ">
           <div className="relative">
-<<<<<<< HEAD
-            <h2 className="pt-4 pb-8 font-bold text-2xl text-black">كتاب لعبة المال</h2>
+            <h2 className="pt-4 pb-8 text-2xl text-black">دعم الصم</h2>
+            <p>في مجتمعنا هناك فئات عديدة من ذوي الحاجات الخاصة, ونظرا لضعف الشباب في العموم في وطننا العربي هذة الفئة مهمشة عند ذكر أولويات التنمية المجتمعية بالنسبة لاكادميتنا وجدنا أن بامكاننا دعم أحد هذه الفئات وهي فئة الصم بتحويل اجزاء من محتوى الاكاديمية المرئي والمسموع ليكون أيضا قابل للقراءة أثناء عرض المحتوى, وبهذا تمتد النهضة في مجتمعنا الى كل الفئات</p>
+            <button className="font-bold text-2xl bg-[#FBD675] px-[55px] py-[4px] mt-[0px] align-middle cursor-pointer	rounded-[10px]">2019</button>
+          </div>
+        </div>
+
+        <div className="float-left p-[25px] mt-[100px] mr-[20px] mb-[50px] ml-[20px] w-1/4 text-center	rounded-[20px] shadow-3xl ">
+          <div className="relative">
+            <h2 className="pt-4 pb-8 text-2xl text-black">تحدي الستين</h2>
+            <p>فعاليـة دوريـة لا تتوقف منذ عـام 2019 الفعالية ترعى الشباب يوم بيوم لمحاولة الوصول لهدف مع مراعاة الحفاظ على أساسيات اللعبة اليومية <br /> استيقاظ مبكر، صلاة، رياضة، تعلم، هدفبهذه الخمس أساسيات نضمن فرد منتج في يومه ونرافقه لستين يومـًا نغير بها حياته </p>
+            <button className="font-bold text-2xl bg-[#FBD675] px-[55px] py-[4px] mt-[80px] align-middle cursor-pointer	rounded-[10px]">2019</button>
+          </div>
+        </div>
+
+        <div className="float-left p-[25px] mt-[100px] mr-[20px] mb-[50px] ml-[20px] w-1/4 text-center	rounded-[20px] shadow-3xl ">
+          <div className="relative">
+            <h2 className="pt-4 pb-8 text-2xl text-black">كتاب لعبة المال</h2>
             <p>كتاب ببحث شامـل من ٥٠٠ صفحـة لكـل ما يحتاجه الفرد للعمـل على الانتـرنت والتطـور لامتـلاك عملــه الخـاص من الصفـر،<br /> الكتـاب يراعي المبتدئيـن ويهـدف لتثقيـف طلابنـا بالمـال وكيفية توليده وحفظه و استثماره.</p>
-            <button className="font-bold text-2xl bg-[#FBD675] px-[55px] py-[4px] mt-[85px] align-middle cursor-pointer	rounded-[10px]">2021</button>
-            <h3 className="absolute -my-[-40px] right-0  left-0 text-[#2995F8]">... للمزيد عن هذا المشروع</h3>
-=======
-          <h2 className="pt-4 pb-8 text-2xl text-black">دعم الصم</h2>
-          <p>في مجتمعنا هناك فئات عديدة من ذوي الحاجات الخاصة, ونظرا لضعف الشباب في العموم في وطننا العربي هذة الفئة مهمشة عند ذكر أولويات التنمية المجتمعية بالنسبة لاكادميتنا وجدنا أن بامكاننا دعم أحد هذه الفئات وهي فئة الصم بتحويل اجزاء من محتوى الاكاديمية المرئي والمسموع ليكون أيضا قابل للقراءة أثناء عرض المحتوى, وبهذا تمتد النهضة في مجتمعنا الى كل الفئات</p>
-          <button className="font-bold text-2xl bg-[#FBD675] px-[55px] py-[4px] mt-[0px] align-middle cursor-pointer	rounded-[10px]">2019</button>
->>>>>>> dc5070c9696d0b6211bf9f8ca5cfc6338a02ec46
-          </div>
-        </div>
-
-        <div className="float-left p-[25px] mt-[100px] mr-[20px] mb-[50px] ml-[20px] w-1/4 text-center	rounded-[20px] shadow-3xl ">
-          <div className="relative">
-<<<<<<< HEAD
-            <h2 className="pt-4 pb-8 font-bold text-2xl text-black">تحدي الستين</h2>
-            <p>فعاليـة دوريـة لا تتوقف منذ عـام 2019 الفعالية ترعى الشباب يوم بيوم لمحاولة الوصول لهدف مع مراعاة الحفاظ على أساسيات اللعبة اليومية استيقاظ مبكر، صلاة، رياضة، تعلم، هدفبهذه الخمس أساسيات نضمن فرد منتج في يومه ونرافقه لستين يومـًا نغير بها حياته </p>
-            <button className="font-bold text-2xl bg-[#FBD675] px-[55px] py-[4px] mt-[40px] align-middle cursor-pointer	rounded-[10px]">2021</button>
-            <h3 className="absolute -my-[-40px] right-0  left-0 text-[#2995F8]">... للمزيد عن هذا المشروع</h3>
-          </div>
-        </div>
-
-
-      </div>
-    </div>
-=======
-          <h2 className="pt-4 pb-8 text-2xl text-black">تحدي الستين</h2>
-          <p>فعاليـة دوريـة لا تتوقف منذ عـام 2019 الفعالية ترعى الشباب يوم بيوم لمحاولة الوصول لهدف مع مراعاة الحفاظ على أساسيات اللعبة اليومية <br/> استيقاظ مبكر، صلاة، رياضة، تعلم، هدفبهذه الخمس أساسيات نضمن فرد منتج في يومه ونرافقه لستين يومـًا نغير بها حياته </p>
-           <button className="font-bold text-2xl bg-[#FBD675] px-[55px] py-[4px] mt-[80px] align-middle cursor-pointer	rounded-[10px]">2019</button>
-          </div>
-        </div>
-
-        <div className="float-left p-[25px] mt-[100px] mr-[20px] mb-[50px] ml-[20px] w-1/4 text-center	rounded-[20px] shadow-3xl ">
-          <div className="relative">
-          <h2 className="pt-4 pb-8 text-2xl text-black">كتاب لعبة المال</h2>
-            <p>كتاب ببحث شامـل من ٥٠٠ صفحـة لكـل ما يحتاجه الفرد للعمـل على الانتـرنت والتطـور لامتـلاك عملــه الخـاص من الصفـر،<br/> الكتـاب يراعي المبتدئيـن ويهـدف لتثقيـف طلابنـا بالمـال وكيفية توليده وحفظه و استثماره.</p>
             <button className="font-bold text-2xl bg-[#FBD675] px-[55px] py-[4px] mt-[120px] align-middle cursor-pointer	rounded-[10px]">2020</button>
           </div>
         </div>
-      </div> 
-    </div> 
->>>>>>> dc5070c9696d0b6211bf9f8ca5cfc6338a02ec46
+      </div>
+    </div>
   )
 }
 
